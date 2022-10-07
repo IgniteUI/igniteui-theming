7bdbe478d7a900811f4fba0c3ef8dbaf29296fac
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-10-07 17:25:24 +0300

    build(json): add palette and typography presets

--- scripts/buildJSON.mjs
+++ scripts/buildJSON.mjs
@@ -10,37 +10,125 @@ import { globby } from "globby";
 const exec = promisify(_exec);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PALETTE_PRESETS = "sass/color/presets";
+const TYPOGRAPHY_PRESETS = "sass/typography/presets";
 const DEST_DIR = path.join.bind(null, path.resolve(__dirname, "../json"));
 
 async function palettesToJSON() {
   const paths = await globby(`${PALETTE_PRESETS}/{light,dark}/*.scss`);
   const palettes = {
     inputFiles: paths,
-    includePaths: [`${PALETTE_PRESETS}/light/`, `${PALETTE_PRESETS}dark/`],
+    includePaths: [`${PALETTE_PRESETS}/light/`, `${PALETTE_PRESETS}/dark/`],
   };
-  const p = exporter(palettes).getStructured();
-  const outputFile = DEST_DIR("palettes.json");
+  const data = exporter(palettes).getStructured();
+  const outputFile = DEST_DIR("colors/presets/palettes.json");
+  const normalized = Object.entries(data).reduce((acc, next) => {
+    const [key, value] = next;
+    if (key !== "variables") {
+      value.forEach((palette) => {
+        acc[`${key}-${palette.name.substring(1)}`] = palette.mapValue.reduce((acc, color) => {
+          if (color.name === "variant") return acc;
+          acc[color.name] = color.value;
+          return acc;
+        }, {});
+      });
+    }
+    return acc;
+  }, {});
   makeDir(path.dirname(outputFile), { recursive: true });
-  writeFile(outputFile, JSON.stringify(p), "utf-8");
+  writeFile(outputFile, JSON.stringify(normalized), "utf-8");
+}
+
+async function typographyToJSON() {
+  const paths = await globby(`${TYPOGRAPHY_PRESETS}/*.scss`);
+  const presets = {
+    inputFiles: paths,
+    includePaths: [`${TYPOGRAPHY_PRESETS}`],
+  };
+  const data = exporter(presets).getStructured();
+  const normalized = Object.entries(data).reduce((acc, next) => {
+    const [key, value] = next;
+    if (key !== "variables") {
+      acc[key] = value.reduce((styles, variable) => {
+        if (variable.name !== "$type-scale" && variable.name !== "$_base-scale") {
+          styles[variable.name.substring(1)] =
+            variable.mapValue?.reduce((acc, next) => {
+              acc[next.name] = next.compiledValue;
+              return acc;
+            }, {}) || variable.value;
+        }
+        return styles;
+      }, {});
+    }
+    return acc;
+  }, {});
+  const outputFile = DEST_DIR("typography/presets/typescales.json");
+  makeDir(path.dirname(outputFile), { recursive: true });
+  writeFile(outputFile, JSON.stringify(normalized), "utf-8");
 }
 
 async function colorsToJSON() {
   const options = {
     inputFiles: ["sass/color/_types.scss", "sass/color/_multipliers.scss"],
   };
-  const colors = exporter(options).getArray();
-  const outputFile = DEST_DIR("colors.json");
-  makeDir(path.dirname(outputFile), { recursive: true });
-  writeFile(outputFile, JSON.stringify(colors), "utf-8");
+  const data = exporter(options).getArray();
+  const color = data.find((d) => d.name === "$color").mapValue;
+  const grayscale = data.find((d) => d.name === "$grayscale").mapValue;
+  const color_s = color.find((d) => d.name === "s").mapValue;
+  const color_l = color.find((d) => d.name === "l").mapValue;
+  const grayscale_l = grayscale.find((d) => d.name === "l").mapValue;
+  const gray_shades = data.find((d) => d.name === "$IGrayShades").compiledValue.split(/\s*,\s*/);
+  const color_shades = data.find((d) => d.name === "$IColorShades").compiledValue.split(/\s*,\s*/);
+
+  const multipliers = {
+    color: {
+      s: color_s.reduce((acc, next) => {
+        acc[next.name] = next.value;
+        return acc;
+      }, {}),
+      l: color_l.reduce((acc, next) => {
+        acc[next.name] = next.value;
+        return acc;
+      }, {}),
+    },
+    grayscale: {
+      l: grayscale_l.reduce((acc, next) => {
+        acc[next.name] = next.value;
+        return acc;
+      }, {}),
+    },
+  };
+  const palette = data
+    .find((d) => d.name === "$IPaletteColors")
+    .mapValue.reduce((acc, next) => {
+      if (next.name === "gray") {
+        acc[next.name] = gray_shades;
+        return acc;
+      }
+
+      acc[next.name] = color_shades;
+      return acc;
+    }, {});
+
+  const multipliersFile = DEST_DIR("colors/meta/multipliers.json");
+  const paletteFile = DEST_DIR("colors/meta/palette.json");
+
+  makeDir(path.dirname(multipliersFile), { recursive: true });
+  makeDir(path.dirname(paletteFile), { recursive: true });
+
+  writeFile(multipliersFile, JSON.stringify(multipliers), "utf-8");
+  writeFile(paletteFile, JSON.stringify(palette), "utf-8");
 }
 
 (async () => {
   await exec("npm run clean");
 
-  console.info("Building JSON palettes...");
+  console.info("Exporting palette presets to JSON...");
   await palettesToJSON();
 
-  console.info("Building JSON colors...");
+  console.info("Exporting typography presets to JSON...");
+  await typographyToJSON();
+
+  console.info("Exporting color metadata to JSON...");
   await colorsToJSON();
 
   console.log(`Done! 🎉`);
