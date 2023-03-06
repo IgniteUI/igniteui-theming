4257d02cd49003e0f2e3517bf7fa60d163e39ff5
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-03-06 15:48:52 +0200

    build(sass): update deps and lint configs

--- test/test.js
+++ test/test.js
@@ -2,4 +2,9 @@ const path = require('path');
 const sassTrue = require('sass-true');
 
 const sassFile = path.join(__dirname, '_index.scss');
-sassTrue.runSass({ file: sassFile }, { describe, it });
+
+sassTrue.runSass(
+  { describe, it },
+  sassFile,
+  { loadPaths: ['node_modules'] }
+);
