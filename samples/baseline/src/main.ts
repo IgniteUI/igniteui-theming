993d85e40dbcd680a050b136d81ad0b3550191f9
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-05-11 14:44:08 +0300

    samples(baseline): add baseline sample

---
+++ samples/baseline/src/main.ts
@@ -0,0 +1,10 @@
+import './style.scss'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Ignite UI Theming</h1>
+    <p class="read-the-docs">
+      Click on the Ignite UI logo to learn more
+    </p>
+  </div>
+`
