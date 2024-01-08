56edcbe0c69eb4c3b2dd68b66b9b248708eaa657
didimmova <d.dimova11@gmail.com>
2024-01-08 15:46:00 +0200

    Revert "refactor(schemas): refactor schemas from a to c"
    This reverts commit 5a575aaa0757c4d7d052042338b352ef610d911c.

--- package/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Palettes } from "./json/colors/presets/palettes.json";
-export { default as PaletteMeta } from "./json/colors/meta/palette.json";
-export { default as PaletteMultipliers } from "./json/colors/meta/multipliers.json";
-export { default as Typescales } from "./json/typography/presets/typescales.json";
-export { default as Elevations } from "./json/elevations/elevations.json";
